94424e2751e8620a25d732b567230843b701ec7f
unknownMusician <serega3assassin2002@gmail.com>
2020-09-30 21:42:49 +0300

    GameL

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameLogic : MonoBehaviour {
-    public static KitchenLogic kitchen;
-    public static RestaurantLogic restaurant;
+
+    public static GameLogic L { get; protected set; }
+    public static KitchenLogic Kitchen { get; protected set; }
+    public static RestaurantLogic Restaurant { get; protected set; }
 
     protected void Awake() {
-        kitchen = GetComponent<KitchenLogic>();
-        restaurant = GetComponent<RestaurantLogic>();
+        L = this;
+        Kitchen = GetComponent<KitchenLogic>();
+        Restaurant = GetComponent<RestaurantLogic>();
     }
 }
