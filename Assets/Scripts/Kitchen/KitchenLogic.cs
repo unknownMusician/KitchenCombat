53d1cf5cf720be971d4e73cb7d9c534215703996
unknownMusician <serega3assassin2002@gmail.com>
2020-11-03 22:33:16 +0200

    added protected modifiers to KitchenLogic

--- Assets/Scripts/Kitchen/KitchenLogic.cs
+++ Assets/Scripts/Kitchen/KitchenLogic.cs
@@ -7,11 +7,11 @@ public class KitchenLogic : MonoBehaviour {
     public Transform GameMenu { get; set; } = default;
     public RectTransform UIMenu { get; set; } = default;
 
-    public Main main = default; // todo: unused
-    public ComboManager combo = default;
+    protected Main main = default; // todo: unused
+    protected ComboManager combo = default;
     public Relations relations = default;
-    public OrderManager orderManager = default;
-    public DishCreator dishCreator = default;
+    protected OrderManager orderManager = default;
+    protected DishCreator dishCreator = default;
 
     // todo: unused
     [System.Serializable] public class Main {
